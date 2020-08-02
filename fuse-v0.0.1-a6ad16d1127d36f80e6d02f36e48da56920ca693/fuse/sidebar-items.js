221697162e86e6514fe5ad62942cf83c205cd962
John Millikin <john@john-millikin.com>
2020-08-02 12:59:46 +0900

    fuse-v0.0.1-a6ad16d1127d36f80e6d02f36e48da56920ca693

---
+++ fuse-v0.0.1-a6ad16d1127d36f80e6d02f36e48da56920ca693/fuse/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["os",""],["protocol",""]],"struct":[["FuseServer",""],["FuseServerBuilder",""],["FuseServerExecutor",""],["ProtocolVersion",""],["ServerContext",""]],"trait":[["FuseHandlers","User-provided handlers for FUSE operations."],["FuseMount",""],["FuseMountOptions",""],["RespondOnce",""],["RespondOnceBox",""]]});
---
+++ fuse-v0.0.1-a6ad16d1127d36f80e6d02f36e48da56920ca693/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fuse"] = [{"text":"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"fuse/protocol/struct.Dirent.html\" title=\"struct fuse::protocol::Dirent\">Dirent</a>&lt;'_&gt;","synthetic":false,"types":["fuse::protocol::readdir::Dirent"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
