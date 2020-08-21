jmillikin/rust-fuse

commit a63571d8a6e2b55b79378058d7ccdde3c371e292

Put nightly-dependent functionality behind non-default features.

fuse/src/channel.rs
@@ -15,6 +15,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use core::convert::TryInto;
+
+#[cfg(feature = "nightly_impl_channel")]
 use core::mem::{self, MaybeUninit};
 
 #[cfg(feature = "std")]
@@ -22,6 +24,7 @@ use std::io::{self, IoSlice, Read, Write};
 
 use crate::error::{Error, ErrorCode};
 
+#[cfg(feature = "nightly_impl_channel")]
 pub trait Channel {
 	type Error: ChannelError;
 
@@ -31,6 +34,26 @@ pub trait Channel {
 		&self,
 		bufs: &[&[u8]; N],
 	) -> Result<(), Self::Error>;
+
+	fn receive(&self, buf: &mut [u8]) -> Result<usize, Self::Error>;
+}
+
+#[cfg(not(feature = "nightly_impl_channel"))]
+pub(crate) mod private {
+	pub trait ChannelNoConstGenerics<Error> {
+		fn send_vectored_2(&self, bufs: &[&[u8]; 2]) -> Result<(), Error>;
+		fn send_vectored_3(&self, bufs: &[&[u8]; 3]) -> Result<(), Error>;
+		fn send_vectored_5(&self, bufs: &[&[u8]; 5]) -> Result<(), Error>;
+	}
+}
+
+#[cfg(not(feature = "nightly_impl_channel"))]
+pub trait Channel:
+	private::ChannelNoConstGenerics<<Self as Channel>::Error>
+{
+	type Error: ChannelError;
+
+	fn send(&self, buf: &[u8]) -> Result<(), Self::Error>;
 
 	fn receive(&self, buf: &mut [u8]) -> Result<usize, Self::Error>;
 }
@@ -57,6 +80,73 @@ impl FileChannel {
 	}
 }
 
+#[cfg(not(feature = "nightly_impl_channel"))]
+impl private::ChannelNoConstGenerics<io::Error> for FileChannel {
+	fn send_vectored_2(&self, bufs: &[&[u8]; 2]) -> Result<(), io::Error> {
+		let mut bufs_len: usize = 0;
+		for buf in bufs {
+			bufs_len += buf.len();
+		}
+		let write_size = Write::write_vectored(
+			&mut &self.file,
+			&[IoSlice::new(bufs[0]), IoSlice::new(bufs[1])],
+		)?;
+		if write_size < bufs_len {
+			return Err(io::Error::new(
+				io::ErrorKind::Other,
+				"incomplete send",
+			));
+		}
+		Ok(())
+	}
+
+	fn send_vectored_3(&self, bufs: &[&[u8]; 3]) -> Result<(), io::Error> {
+		let mut bufs_len: usize = 0;
+		for buf in bufs {
+			bufs_len += buf.len();
+		}
+		let write_size = Write::write_vectored(
+			&mut &self.file,
+			&[
+				IoSlice::new(bufs[0]),
+				IoSlice::new(bufs[1]),
+				IoSlice::new(bufs[2]),
+			],
+		)?;
+		if write_size < bufs_len {
+			return Err(io::Error::new(
+				io::ErrorKind::Other,
+				"incomplete send",
+			));
+		}
+		Ok(())
+	}
+
+	fn send_vectored_5(&self, bufs: &[&[u8]; 5]) -> Result<(), io::Error> {
+		let mut bufs_len: usize = 0;
+		for buf in bufs {
+			bufs_len += buf.len();
+		}
+		let write_size = Write::write_vectored(
+			&mut &self.file,
+			&[
+				IoSlice::new(bufs[0]),
+				IoSlice::new(bufs[1]),
+				IoSlice::new(bufs[2]),
+				IoSlice::new(bufs[3]),
+				IoSlice::new(bufs[4]),
+			],
+		)?;
+		if write_size < bufs_len {
+			return Err(io::Error::new(
+				io::ErrorKind::Other,
+				"incomplete send",
+			));
+		}
+		Ok(())
+	}
+}
+
 #[cfg(feature = "std")]
 impl Channel for FileChannel {
 	type Error = io::Error;
@@ -72,6 +162,7 @@ impl Channel for FileChannel {
 		Ok(())
 	}
 
+	#[cfg(feature = "nightly_impl_channel")]
 	fn send_vectored<const N: usize>(
 		&self,
 		bufs: &[&[u8]; N],

examples/helloworld/Cargo.toml
@@ -12,3 +12,6 @@ path = "helloworld.rs"
 [dependencies]
 fuse = { version = "0.0.1", path = "../../fuse" }
 libc = "0.2"
+
+[features]
+default = ["fuse/libc_fuse_mount"]

examples/helloworld/helloworld.rs
@@ -196,7 +196,7 @@ fn main() {
 	let handlers = HelloWorldFS {};
 	let mut srv = linux::FuseServerBuilder::new(&mount_target, handlers)
 		.set_mount(
-			linux::SyscallFuseMount::new()
+			linux::LibcFuseMount::new()
 				.set_mount_source("helloworld")
 				.set_mount_subtype("helloworld"),
 		)

examples/helloworld_async/Cargo.toml
@@ -12,3 +12,6 @@ path = "helloworld_async.rs"
 [dependencies]
 fuse = { version = "0.0.1", path = "../../fuse" }
 libc = "0.2"
+
+[features]
+default = ["fuse/libc_fuse_mount"]

examples/helloworld_async/helloworld_async.rs
@@ -221,7 +221,7 @@ fn main() {
 	let handlers = HelloWorldFS {};
 	let mut srv = linux::FuseServerBuilder::new(&mount_target, handlers)
 		.set_mount(
-			linux::SyscallFuseMount::new()
+			linux::LibcFuseMount::new()
 				.set_mount_source("helloworld")
 				.set_mount_subtype("helloworld"),
 		)

fuse/Cargo.toml
@@ -8,9 +8,17 @@ edition = "2018"
 [lib]
 name = "fuse"
 
+[dependencies]
+libc = { version = "0.2.*", optional = true }
+
 [features]
 default = ["std"]
 std = []
+
+libc_fuse_mount = ["libc"]
+
+nightly_impl_channel = []
+nightly_syscall_fuse_mount = []
 
 unstable_access = []
 unstable_bmap = []

fuse/src/fuse_server.rs
@@ -20,7 +20,7 @@ use core::cmp;
 #[cfg(feature = "std")]
 use std::sync::Arc;
 
-use crate::channel::{self, ChannelError};
+use crate::channel;
 use crate::error::{Error, ErrorCode};
 use crate::fuse_handlers::FuseHandlers;
 use crate::internal::fuse_io::{

fuse/src/internal/fuse_io.rs
@@ -418,7 +418,14 @@ impl<Chan: Channel> ResponseEncoder<'_, Chan> {
 				size_of::<fuse_kernel::fuse_out_header>(),
 			)
 		};
-		self.channel.send_vectored(&[out_hdr_buf, bytes])
+
+		#[cfg(feature = "nightly_impl_channel")]
+		let rc = self.channel.send_vectored(&[out_hdr_buf, bytes]);
+
+		#[cfg(not(feature = "nightly_impl_channel"))]
+		let rc = self.channel.send_vectored_2(&[out_hdr_buf, bytes]);
+
+		rc
 	}
 
 	pub(crate) fn encode_bytes_2(
@@ -454,7 +461,16 @@ impl<Chan: Channel> ResponseEncoder<'_, Chan> {
 				size_of::<fuse_kernel::fuse_out_header>(),
 			)
 		};
-		self.channel.send_vectored(&[out_hdr_buf, bytes_1, bytes_2])
+
+		#[cfg(feature = "nightly_impl_channel")]
+		let rc = self.channel.send_vectored(&[out_hdr_buf, bytes_1, bytes_2]);
+
+		#[cfg(not(feature = "nightly_impl_channel"))]
+		let rc = self
+			.channel
+			.send_vectored_3(&[out_hdr_buf, bytes_1, bytes_2]);
+
+		rc
 	}
 
 	pub(crate) fn encode_bytes_4(
@@ -500,12 +516,25 @@ impl<Chan: Channel> ResponseEncoder<'_, Chan> {
 				size_of::<fuse_kernel::fuse_out_header>(),
 			)
 		};
-		self.channel.send_vectored(&[
+
+		#[cfg(feature = "nightly_impl_channel")]
+		let rc = self.channel.send_vectored(&[
 			out_hdr_buf,
 			bytes_1,
 			bytes_2,
 			bytes_3,
 			bytes_4,
-		])
+		]);
+
+		#[cfg(not(feature = "nightly_impl_channel"))]
+		let rc = self.channel.send_vectored_5(&[
+			out_hdr_buf,
+			bytes_1,
+			bytes_2,
+			bytes_3,
+			bytes_4,
+		]);
+
+		rc
 	}
 }

commit ced54bbf00f2b37c7a038ac7833d40a140c7443f

Fix no_std build (broken by a63571d)

fuse/src/channel.rs
@@ -80,6 +80,7 @@ impl FileChannel {
 	}
 }
 
+#[cfg(feature = "std")]
 #[cfg(not(feature = "nightly_impl_channel"))]
 impl private::ChannelNoConstGenerics<io::Error> for FileChannel {
 	fn send_vectored_2(&self, bufs: &[&[u8]; 2]) -> Result<(), io::Error> {